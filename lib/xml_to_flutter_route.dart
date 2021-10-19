1eecb514df24534e06f0c969ddcc8938fac613e7
Hendra Marihot <hendra.marihot@gmail.com>
2021-10-20 00:05:25 +0700

    add classes

--- lib/xml_to_flutter_route.dart
+++ lib/xml_to_flutter_route.dart
@@ -1,7 +1,39 @@
 library xml_to_flutter_route;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+abstract class XmlWidgetElement {
+  Widget construct(BuildContext context);
+}
+
+abstract class XmlWidget extends StatelessWidget implements XmlWidgetElement {
+  XmlWidget({Key? key}) : super(key: key);
+  final Map<String, XmlWidgetElement> params = {};
+
+  void addParam(String paramName, XmlWidgetElement value);
+
+  @override
+  Widget construct(BuildContext context);
+}
+
+class XmlWidgetScaffold extends XmlWidget {
+  XmlWidgetScaffold({Key? key}) : super(key: key);
+
+  @override
+  void addParam(String paramName, XmlWidgetElement value) {
+    params.addAll({paramName: value});
+  }
+
+  @override
+  Widget construct(BuildContext context) {
+    return Scaffold(
+      appBar: params['param_appbar']?.construct(context) as AppBar,
+      body: params['param_body']?.construct(context),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return construct(context);
+  }
 }
